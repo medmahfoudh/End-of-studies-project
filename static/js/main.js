230dd1e6526528de9fe3dd03318faa9277e1ec74
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-29 11:00:10 +0100

    template and Static Folder...

---
+++ static/js/main.js
@@ -0,0 +1,61 @@
+//===========SCROLL ANIMATION============//
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+  
+  window.addEventListener("scroll", reveal);
+  
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
+
+
+
+  // ==============MENU PHONE============
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".menu-me nav");
+const links = document.querySelectorAll("li a");
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
