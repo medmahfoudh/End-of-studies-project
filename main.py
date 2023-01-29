4a0b19a7d5e5d7c43b903951a5abbc660764a7ff
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-29 10:13:02 +0100

    App Setup

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from flask import Flask
-import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'This is My First App in Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
