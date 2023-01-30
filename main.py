de1dfbefcdf356b25f6fcdf001626707c4450590
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-30 15:07:49 +0100

    30-1-022 V1

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def home():
 def sign_in():
     return render_template('sign_in.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/candidate_form')
+def candidate_form():
+    return render_template('candidate_form.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
