7098d0dc95bd4b43eed935c4f7d5cc74852d8e37
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-02-05 15:56:01 +0100

    Add collection job_applicats

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017")
+db = client['SmartRecruiter']
+collection = db['job_applicant']
+
 
 @app.route('/')
 def home():
